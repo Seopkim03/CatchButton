99852125d00db6bcb95d4fa86380270ee66fae5f
김현섭 <>
2026-03-12 11:31:52 +0900

    "나를 잡아봐" 버튼 생성, 마우스가 버튼으로 들어가면 100, 150 좌표로 버튼이 이동.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            RunningButton = new Button();
+            SuspendLayout();
+            // 
+            // RunningButton
+            // 
+            RunningButton.BackColor = SystemColors.ActiveCaptionText;
+            RunningButton.Font = new Font("맑은 고딕", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            RunningButton.ForeColor = SystemColors.ButtonFace;
+            RunningButton.Location = new Point(100, 153);
+            RunningButton.Name = "RunningButton";
+            RunningButton.Size = new Size(305, 82);
+            RunningButton.TabIndex = 0;
+            RunningButton.Text = "나를 잡아봐";
+            RunningButton.UseVisualStyleBackColor = false;
+            RunningButton.MouseEnter += RunningButton_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(RunningButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button RunningButton;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,15 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void RunningButton_MouseEnter(object sender, EventArgs e)
+        {
+            int x_position = 100; //x좌표를 100으로 설정
+            int y_position = 150; //y좌표를 150으로 설정
+
+            RunningButton.Location = new Point(x_position, y_position);//나를 잡아봐 버튼의 좌표
+        }
+
+        
     }
 }
